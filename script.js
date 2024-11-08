6f67ccca57c0a4902228bf6f248501f8ab0cd2aa
Feliphe-Oliveira <oliveirafeliphe272@gmail.com>
2024-11-07 21:45:23 -0300

    concerto

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('btnAgu').addEventListener('click',()=>{
-    const cod = 1212
-    if(document.getElementById('senha').value==cod){
+    const cod = 'dor'
+    if(document.getElementById('senha').value.toUpperCase()==cod.toUpperCase()){
         document.getElementById('cx_verso').classList.add('destaque')
         document.getElementById('login').classList.add('semdestaque')
     } else if(document.getElementById('senha').value == ''){
